921fa19d5a2e2677b007236506e9d74c8cc6d0be
John Corser <johnpc@umich.edu>
2014-04-23 17:41:49 -0400

    fix database save for sqlite and pg

--- Gemfile
+++ Gemfile
@@ -22,10 +22,11 @@ group :doc do
   gem 'sdoc', require: false
 end
 
-group :development do
-    gem 'sqlite3'
-end
+
 group :production do
     gem 'pg'
     gem 'rails_12factor'
+end
+group :development do
+    gem 'sqlite3'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   acts_as_messageable
-#  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 
 #  def name
 #      email
